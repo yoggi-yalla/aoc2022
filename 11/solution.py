ff6382f33a86ae347913afa3b2aef42434b52353
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2022-12-11 09:26:12 +0100

    day 11

--- 11/solution.py
+++ 11/solution.py
@@ -1,45 +1,42 @@
-from collections import defaultdict
 from collections import deque
-import math
+from copy import deepcopy
 
 with open('input.txt') as f:
     data = f.read()
 
 
-def parse_data(data):
-    monkeys = {}
-    gcd = 1
-    groups = data.split('\n\n')
-    for i, g in enumerate(groups):
-        lines = g.splitlines()
-        items = eval("[" + lines[1].split(': ')[1] + "]")
-        items = deque(items)
-        op = lines[2].split(': ')[1].replace('old', 'item').replace('new', 'item')
-        test = int(lines[3].split()[-1])
-        t = int(lines[4].split()[-1])
-        f = int(lines[5].split()[-1])
-        gcd *= test
+groups = data.split('\n\n')
 
-        monkeys[i] = (items, op, test, t, f)
-    return monkeys, gcd
+monkeys = []
+counts = [0] * len(groups)
+gcd = 1
 
 
-def monkey_business(business):
-    return math.prod(sorted(business.values(), reverse=True)[:2])
+for i, g in enumerate(groups):
+    lines = g.splitlines()
+
+    items = eval("[" + lines[1].split(': ')[1] + "]")
+    items = deque(items)
+    op = eval("lambda old:" + lines[2].split('=')[1])
+    test = int(lines[3].split()[-1])
+    t = int(lines[4].split()[-1])
+    f = int(lines[5].split()[-1])
+
+    gcd *= test
+    monkeys.append((items, op, test, t, f))
 
 
-for part in (1,2):
-    monkeys, gcd = parse_data(data)
-    business = defaultdict(int)
-    for n in range(20 if part == 1 else 10000):
-        for i in range(len(monkeys)):
-            m = monkeys[i]
+
+def run(monkeys, counts, iterations, part_1):
+    for _ in range(iterations):
+        for i, m in enumerate(monkeys):
             items, op, test, t, f = m
-            for _ in range(len(items)):
-                item = items.popleft()
-                exec(op)
 
-                if part == 1:
+            while items:
+                item = items.popleft()
+                item = op(item)
+
+                if part_1 == True:
                     item //= 3
                 else:
                     item %= gcd
@@ -49,6 +46,16 @@ for part in (1,2):
                 else:
                     monkeys[f][0].append(item)
 
-                business[i] += 1
+                counts[i] += 1
 
-    print(f"Part {part}:", monkey_business(business))
+    counts.sort(reverse=True)
+
+    return counts[0] * counts[1]
+
+
+monkeys2 = deepcopy(monkeys)
+counts2 = deepcopy(counts)
+
+
+print("Part 1:", run(monkeys, counts, 20, True))
+print("Part 2:", run(monkeys2, counts2, 10000, False))
