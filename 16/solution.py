20d398b0fc5215098c218a5e310b14951eaa51d6
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2022-12-17 15:12:06 +0100

    day 16

--- 16/solution.py
+++ 16/solution.py
@@ -1,61 +1,9 @@
 from collections import deque
+import itertools
 import re
 
-pattern = re.compile(r".*?([A-Z]+).*?(\d+)")
-
-with open('input.txt') as f:
-    data = f.read()
-
-
-data = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-Valve BB has flow rate=13; tunnels lead to valves CC, AA
-Valve CC has flow rate=2; tunnels lead to valves DD, BB
-Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-Valve EE has flow rate=3; tunnels lead to valves FF, DD
-Valve FF has flow rate=0; tunnels lead to valves EE, GG
-Valve GG has flow rate=0; tunnels lead to valves FF, HH
-Valve HH has flow rate=22; tunnel leads to valve GG
-Valve II has flow rate=0; tunnels lead to valves AA, JJ
-Valve JJ has flow rate=21; tunnel leads to valve II"""
-
-
-
-flow_rates = {}
-connections = {}
-rooms = []
-rates = []
-
-
-
-
-for line in data.splitlines():
-    f, r = re.match(pattern, line[1:]).groups()
-
-    if 'valves' in line:
-        t = line.split('valves ')[1]
-        t = tuple(t.split(', '))
-    else:
-        t = line.split()[-1]
-        t = (t,)
-
-    r = int(r)
-
-    flow_rates[f] = r
-    connections[f] = t
-    rooms.append(f)
-    rates.append(r)
-
-
-
-rooms2 = []
-rates2 = []
-for room, rate in zip(rooms, rates):
-    if rate != 0:
-        rooms2.append(room)
-        rates2.append(rate)
-    
-
-
+with open('input.txt') as room:
+    data = room.read()
 
 
 def shortest_path(r1, r2):
@@ -75,63 +23,102 @@ def shortest_path(r1, r2):
         if current == r2:
             return path + (current,)
 
-        for c in connections[current]:
+        for c in room_map[current]:
             q.append((c, path + (current,)))
 
+
+def get_possible_paths(available_rooms, time_limit):
+    possible_paths = []
+
+    q = [("AA", frozenset(), (), 1)]
+    q = deque(q)
+
+    while q:
+        current, visited, path, clock = q.popleft()
+
+        if len(path) == len(available_rooms):
+            possible_paths.append(path)
+            continue
+
+        if clock >= time_limit:
+            possible_paths.append(path)
+            continue
+
+        for rate in available_rooms:
+            if rate not in visited:
+                l = shortest_path(current, rate)
+                q.append((rate, visited | frozenset([rate]), path + (rate,), clock + len(l)))
+    
+    return possible_paths
+
+
+def get_best_result_and_path(possible_paths, time_limit):
+    results = []
+
+    for path in possible_paths:    
+        current = "AA"
+        clock = 0
+        velocity = 0
+        pressure = 0
+
+        for room in path:
+            l = shortest_path(current, room)
+            pressure += velocity * min(time_limit - clock, len(l))
+            clock += len(l)
+            velocity += flow_rates[room]
+            current = room
         
-import itertools
+        pressure += max(0, (time_limit - clock)) * velocity
+        results.append((pressure, path))
+    
+    return max(results)
+
+
+# Parse input
+flow_rates = {}
+room_map = {}
+rooms_with_valves = []
+
+for line in data.splitlines():
+
+    room = line.split()[1]
+    rate = int(re.match(r".*?(\d+)", line).groups()[0])
+    connections = tuple(re.split(r"valves? ", line)[1].split(', '))
+
+    flow_rates[room] = rate
+    room_map[room] = connections
+
+    if rate > 0:
+        rooms_with_valves.append(room)
 
 
 shortest_paths = {}
-for r1, r2 in itertools.combinations(rooms2, 2):
-    shortest_paths[r1, r2] = shortest_path(r1, r2)
-    shortest_paths[r2, r1] = shortest_path(r2, r1)
+for r1, r2 in itertools.combinations(rooms_with_valves, 2):
+    sp = shortest_path(r1, r2)
+    shortest_paths[r1, r2] = sp
+    shortest_paths[r2, r1] = sp[::-1]
 
 
+# Part 1
+paths = get_possible_paths(rooms_with_valves, 30)
+result, _ = get_best_result_and_path(paths, 30)
+
+print("Part 1:", result)
 
 
-possible_paths = []
-
-q = [("AA", frozenset(), (), 1)]
-q = deque(q)
-
-while q:
-    current, visited, path, clock = q.popleft()
-
-    if len(path) == len(rooms2):
-        possible_paths.append(path)
-        continue
-
-    if clock > 30:
-        possible_paths.append(path)
-        continue
-
-    for r in rooms2:
-        if r not in visited:
-            l = shortest_path(current, r)
-            q.append((r, visited | frozenset([r]), path + (r,), clock + len(l)))
+# Part 2
+my_paths = get_possible_paths(rooms_with_valves, 26)
+my_result, my_rooms = get_best_result_and_path(my_paths, 26)
 
 
+elephant_rooms = [r for r in rooms_with_valves if r not in my_rooms]
+elephant_paths = get_possible_paths(elephant_rooms, 26)
 
-results = []
-for path in possible_paths:
-    current = "AA"
-    clock = 0
-    velocity = 0
-    pressure = 0
-
-    for room in path:
-        l = shortest_path(current, room)
-        pressure += velocity * min(30-clock, len(l))
-        clock += len(l)
-        velocity += flow_rates[room]
-        current = room
-    
-    pressure += max(0, (30 - clock)) * velocity
-    results.append((pressure,path))
-    
-
-print(max(results))
+elephant_result, elephant_path = get_best_result_and_path(elephant_paths, 26)
 
 
+print("Part 2:", my_result + elephant_result)
 
+
+import time
+print(time.process_time()) # ~3mins  ¯\_(ツ)_/¯ 
