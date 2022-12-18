c523cdee124c69a700822f087416cc2b15184adb
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2022-12-18 14:28:49 +0100

    day 18

--- 18/solution.py
+++ 18/solution.py
@@ -1,11 +1,27 @@
-from collections import defaultdict, deque
+from collections import deque
+import itertools
 
 with open('input.txt') as f:
     data = f.read()
 
 
-def get_surfaces(cube):
-    x, y, z = cube
+# Adding 1 to each pixel index and making the grid slightly larger than the largest
+# pixel indexes to ensure that there is room for "air" to surround the droplet
+
+droplet_pixels = [[int(ch) + 1 for ch in line.split(',')] for line in data.splitlines()]
+
+W = max(itertools.chain([p[0] for p in droplet_pixels])) + 2 
+D = max(itertools.chain([p[1] for p in droplet_pixels])) + 2
+H = max(itertools.chain([p[2] for p in droplet_pixels])) + 2
+
+
+grid = [[['.' for _ in range(W)] for _ in range(D)] for _ in range(H)]
+
+for x, y, z in droplet_pixels:
+    grid[z][y][x] = "#"
+
+
+def get_surfaces(x, y, z):
     return set([
         frozenset([(x, y, z), (x+1, y, z), (x, y+1, z), (x+1, y+1, z)]),
         frozenset([(x, y, z+1), (x+1, y, z+1), (x, y+1, z+1), (x+1, y+1, z+1)]),
@@ -35,68 +51,38 @@ def neighbors(x,y,z):
             yield (xx, yy, zz)
 
 
+### Part 1
+overlapping_surfaces = set()
+droplet_surfaces = set()
 
-# Parse input
-cubes = [[int(ch) + 1 for ch in line.split(',')] for line in data.splitlines()]
-
-max_x = 0
-max_y = 0
-max_z = 0
-for cube in cubes:
-    x, y, z = cube
-    max_x = max(x, max_x)
-    max_y = max(y, max_y)
-    max_z = max(z, max_z)
+for x, y, z in droplet_pixels:
+    pixel_surfaces = get_surfaces(x, y, z)
+    overlapping_surfaces |= pixel_surfaces & droplet_surfaces
+    droplet_surfaces |= pixel_surfaces
 
 
-grid = [[['.' for _ in range(max_x+2)] for _ in range(max_y+2)] for _ in range(max_z+2)]
-
-for cube in cubes:
-    x, y, z = cube
-    grid[z][y][x] = "#"
+hull = droplet_surfaces - overlapping_surfaces
+print("Part 1:", len(hull))        
 
 
-H = len(grid)
-D = len(grid[0])
-W = len(grid[0][0])
-
-
-# Part 1
-overlaps = defaultdict(int)
-all_surfaces = set()
-
-for cube in cubes:
-    surfaces = get_surfaces(cube)
-    for s in surfaces:
-        if s in all_surfaces:
-            overlaps[s] += 1
-    all_surfaces |= surfaces
-
-surface_area = len(all_surfaces) - sum(overlaps.values())
-print("Part 1:", surface_area)
-
-
-# Part 2
+### Part 2
 outer_surfaces = set()
 
-q = [(0,0,0)]
-q = deque(q)
+q = deque([(0, 0, 0)])
 visited = set()
 
 while q:
-    (x,y,z) = q.popleft()
+    x, y, z = q.popleft()
 
-    if (x,y,z) in visited:
+    if (x, y, z) in visited:
         continue
-    visited.add((x,y,z))
-
-    grid[z][y][x] = 'o'
+    visited.add((x, y, z))
 
     for xx, yy, zz in neighbors(x,y,z):
         if grid[zz][yy][xx] == '#':
-            outer_surfaces |= all_surfaces & get_surfaces((x,y,z))
+            outer_surfaces |= droplet_surfaces & get_surfaces(x, y, z)
         else:
-            q.append((xx,yy,zz))
+            q.append((xx, yy, zz))
 
 print("Part 2:", len(outer_surfaces))
 
