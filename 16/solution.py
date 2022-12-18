82d87db1df9cf5d1ac25e419a3adc203e7dad04c
Oskar Petersen <oskar@Oskars-MacBook-Air.local>
2022-12-18 22:54:32 +0100

    day 16

--- 16/solution.py
+++ 16/solution.py
@@ -27,28 +27,21 @@ def shortest_path(r1, r2):
             q.append((c, path + (current,)))
 
 
-def get_possible_paths(available_rooms, time_limit):
+def get_possible_paths(room, available_rooms, time_limit, current_time=1):
+    if not available_rooms:
+        return [(room,)]
+    
+    if current_time >= time_limit:
+        return [(room,)]
+
     possible_paths = []
 
-    q = [("AA", frozenset(), (), 1)]
-    q = deque(q)
+    for next_room in available_rooms:
+        cost = len(shortest_paths[room, next_room])
+        p = get_possible_paths(next_room, available_rooms - frozenset([next_room]), time_limit, current_time + cost)
+        for pp in p:
+            possible_paths.append((room,) + pp)
 
-    while q:
-        current, visited, path, clock = q.popleft()
-
-        if len(path) == len(available_rooms):
-            possible_paths.append(path)
-            continue
-
-        if clock >= time_limit:
-            possible_paths.append(path)
-            continue
-
-        for room in available_rooms:
-            if room not in visited:
-                l = shortest_path(current, room)
-                q.append((room, visited | frozenset([room]), path + (room,), clock + len(l)))
-    
     return possible_paths
 
 
@@ -56,15 +49,15 @@ def get_best_result_and_path(possible_paths, shortest_paths, time_limit):
     results = []
 
     for path in possible_paths:    
-        current = "AA"
+        current = path[0]
         clock = 0
         velocity = 0
         pressure = 0
 
-        for room in path:
-            l = shortest_paths[current, room]
-            pressure += velocity * min(time_limit - clock, len(l))
-            clock += len(l)
+        for room in path[1:]:
+            sp = shortest_paths[current, room]
+            pressure += velocity * min(time_limit - clock, len(sp))
+            clock += len(sp)
             velocity += flow_rates[room]
             current = room
         
@@ -91,6 +84,8 @@ for line in data.splitlines():
     if rate > 0:
         rooms_with_valves.append(room)
 
+rooms_with_valves = frozenset(rooms_with_valves)
+
 
 shortest_paths = {}
 for r1, r2 in itertools.combinations(room_map.keys(), 2):
@@ -99,26 +94,30 @@ for r1, r2 in itertools.combinations(room_map.keys(), 2):
     shortest_paths[r2, r1] = sp[::-1]
 
 
+
 # Part 1
-paths = get_possible_paths(rooms_with_valves, 30)
+paths = get_possible_paths("AA", rooms_with_valves, 30)
 result, _ = get_best_result_and_path(paths, shortest_paths, 30)
 
 print("Part 1:", result)
 
 
 # Part 2
-my_paths = get_possible_paths(rooms_with_valves, 26)
-my_result, my_rooms = get_best_result_and_path(my_paths, shortest_paths, 26)
+best_result = 0
+for i in range(1, len(rooms_with_valves) // 2 + 1):
+    my_potential_rooms = map(frozenset, itertools.combinations(rooms_with_valves, i))
+    for my_rooms in my_potential_rooms:
+        my_paths = get_possible_paths("AA", my_rooms, 26)
+        my_result, my_path = get_best_result_and_path(my_paths, shortest_paths, 26)
+
+        elephant_paths = get_possible_paths("AA", rooms_with_valves - my_rooms, 26)
+        elephant_result, elephant_path = get_best_result_and_path(elephant_paths, shortest_paths, 26)
+
+        best_result = max(best_result, my_result + elephant_result)
 
 
-elephant_rooms = [r for r in rooms_with_valves if r not in my_rooms]
-elephant_paths = get_possible_paths(elephant_rooms, 26)
-
-elephant_result, elephant_path = get_best_result_and_path(elephant_paths, shortest_paths, 26)
-
-
-print("Part 2:", my_result + elephant_result)
+print("Part 2:", best_result)
 
 
 import time
-print(time.process_time()) # ~52s  ¯\_(ツ)_/¯ 
+print(time.process_time()) # ~15mins  ¯\_(ツ)_/¯ 
